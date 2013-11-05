df279ca02487b3587cafee0b07a9459f4f529f57
Roland Bruggmann <brugr9@bfh.ch>
2013-11-05 12:23:15 +0100

    newly setup, and build the project

---
+++ GraphVisualisierung2/src/main/java/ch/bfh/bti7301/hs2013/gravis/App.java
@@ -0,0 +1,13 @@
+package ch.bfh.bti7301.hs2013.gravis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
