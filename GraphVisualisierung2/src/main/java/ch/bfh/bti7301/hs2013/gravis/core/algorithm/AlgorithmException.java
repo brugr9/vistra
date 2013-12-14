9edca49ea5cd8994eb99c8a30325aee76794ef81
Roland Bruggmann <brugr9@bfh.ch>
2013-12-14 23:40:08 +0100

    Exception comments

--- GraphVisualisierung2/src/main/java/ch/bfh/bti7301/hs2013/gravis/core/algorithm/AlgorithmException.java
+++ GraphVisualisierung2/src/main/java/ch/bfh/bti7301/hs2013/gravis/core/algorithm/AlgorithmException.java
@@ -1,48 +1,73 @@
 package ch.bfh.bti7301.hs2013.gravis.core.algorithm;
 
 /**
- * @author Patrick Kofmel (kofmp1@bfh.ch)
- *
+ * Gravis Algorithm Exception<br>
+ * This class extends <code>Exception</code> that indicates conditions that a
+ * reasonable application might want to catch.
+ * 
+ * @author Roland Bruggmann (brugr9@bfh.ch)
+ * 
  */
 public class AlgorithmException extends Exception {
 
-	private static final long serialVersionUID = 3774886120793515985L;
+	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructs a new exception with <code>null</code> as its detail message.
+	 * The cause is not initialised, and may subsequently be initialised by a
+	 * call to {@link #initCause}.
+	 */
 	public AlgorithmException() {
 		super();
 	}
 
 	/**
+	 * Constructs a new exception with the specified detail message. The cause
+	 * is not initialised, and may subsequently be initialised by a call to
+	 * {@link #initCause}.
+	 * 
 	 * @param message
+	 *            the detail message. The detail message is saved for later
+	 *            retrieval by the {@link #getMessage()} method.
 	 */
 	public AlgorithmException(String message) {
-		super(message);
+		super("GRAVIS: " + message);
 	}
 
 	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with <code>cause</code> is
+	 * <i>not</i> automatically incorporated in this exception's detail message.
+	 * 
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the
+	 *            {@link #getMessage()} method).
 	 * @param cause
+	 *            the cause (which is saved for later retrieval by the
+	 *            {@link #getCause()} method). (A <tt>null</tt> value is
+	 *            permitted, and indicates that the cause is nonexistent or
+	 *            unknown.)
+	 */
+	public AlgorithmException(String message, Throwable cause) {
+		super("GRAVIS: " + message, cause);
+	}
+
+	/**
+	 * Constructs a new exception with the specified cause and a detail message
+	 * of <tt>(cause==null ? null : cause.toString())</tt> (which typically
+	 * contains the class and detail message of <tt>cause</tt>). This
+	 * constructor is useful for exceptions that are little more than wrappers
+	 * for other throwables (for example,
+	 * {@link java.security.PrivilegedActionException}).
+	 * 
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the
+	 *            {@link #getCause()} method). (A <tt>null</tt> value is
+	 *            permitted, and indicates that the cause is nonexistent or
+	 *            unknown.)
 	 */
 	public AlgorithmException(Throwable cause) {
 		super(cause);
 	}
-
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public AlgorithmException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public AlgorithmException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 }
--- GraphVisualisierung2/src/main/java/ch/bfh/bti7301/hs2013/gravis/core/graph/GraphException.java
+++ GraphVisualisierung2/src/main/java/ch/bfh/bti7301/hs2013/gravis/core/graph/GraphException.java
@@ -1,49 +1,73 @@
 package ch.bfh.bti7301.hs2013.gravis.core.graph;
 
 /**
- * @author Patrick Kofmel (kofmp1@bfh.ch)
- *
+ * Gravis Graph Exception<br>
+ * This class extends <code>Exception</code> that indicates conditions that a
+ * reasonable application might want to catch.
+ * 
+ * @author Roland Bruggmann (brugr9@bfh.ch)
+ * 
  */
 public class GraphException extends Exception {
 
-	private static final long serialVersionUID = -6462515714563342030L;
+	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructs a new exception with <code>null</code> as its detail message.
+	 * The cause is not initialised, and may subsequently be initialised by a
+	 * call to {@link #initCause}.
+	 */
 	public GraphException() {
 		super();
 	}
 
 	/**
+	 * Constructs a new exception with the specified detail message. The cause
+	 * is not initialised, and may subsequently be initialised by a call to
+	 * {@link #initCause}.
+	 * 
 	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public GraphException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public GraphException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	/**
-	 * @param message
+	 *            the detail message. The detail message is saved for later
+	 *            retrieval by the {@link #getMessage()} method.
 	 */
 	public GraphException(String message) {
-		super(message);
+		super("GRAVIS: " + message);
 	}
 
 	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with <code>cause</code> is
+	 * <i>not</i> automatically incorporated in this exception's detail message.
+	 * 
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the
+	 *            {@link #getMessage()} method).
 	 * @param cause
+	 *            the cause (which is saved for later retrieval by the
+	 *            {@link #getCause()} method). (A <tt>null</tt> value is
+	 *            permitted, and indicates that the cause is nonexistent or
+	 *            unknown.)
+	 */
+	public GraphException(String message, Throwable cause) {
+		super("GRAVIS: " + message, cause);
+	}
+
+	/**
+	 * Constructs a new exception with the specified cause and a detail message
+	 * of <tt>(cause==null ? null : cause.toString())</tt> (which typically
+	 * contains the class and detail message of <tt>cause</tt>). This
+	 * constructor is useful for exceptions that are little more than wrappers
+	 * for other throwables (for example,
+	 * {@link java.security.PrivilegedActionException}).
+	 * 
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the
+	 *            {@link #getCause()} method). (A <tt>null</tt> value is
+	 *            permitted, and indicates that the cause is nonexistent or
+	 *            unknown.)
 	 */
 	public GraphException(Throwable cause) {
 		super(cause);
 	}
-
-	
 }
